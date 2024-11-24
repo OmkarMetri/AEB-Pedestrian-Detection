8d13beb794da5b89c9e5c9b46c1b0cd906cf375c
OmkarMetri <metriomkar56@gmail.com>
2024-11-24 10:31:55 -0800

    remove yolo logging noise

--- yolov8_detect_pedestrians.py
+++ yolov8_detect_pedestrians.py
@@ -6,12 +6,19 @@ class YOLOv8PedestrianDetector:
     def __init__(self, model_path="models/yolov8n.pt"):
         self.model = YOLO(model_path)
         self.classes = [0]
+        self.model_check()
+    
+    def model_check(self):
+        if self.model is None:
+            print("No model loaded! Exiting . . . ")
+            exit(1)
 
     def detect(self, image_path, conf_threshold=0.5):
         results = self.model(
             image_path,
             conf=conf_threshold,
             task="detect",
+            verbose=False
         )
         pedestrians = [ped for ped in results[0].boxes if ped.cls == 0]
 
@@ -20,13 +27,13 @@ class YOLOv8PedestrianDetector:
         else:
             return False
 
-
-pedDetector = YOLOv8PedestrianDetector()
-demo_folder = "demo_test_images"
-for img in os.listdir(demo_folder):
-    img_path = f"{demo_folder}/{img}"
-    result = pedDetector.detect(img_path)
-    if result:
-        print(f"{img_path.upper()}: Pedestrian Detected")
-    else:
-        print(f"{img_path.upper()}: NO Pedestrian Detected")
+if __name__ == "__main__":
+    pedDetector = YOLOv8PedestrianDetector()
+    demo_folder = "demo_test_images"
+    for img in os.listdir(demo_folder):
+        img_path = f"{demo_folder}/{img}"
+        result = pedDetector.detect(img_path)
+        if result:
+            print(f"{img_path.upper()}: Pedestrian Detected")
+        else:
+            print(f"{img_path.upper()}: NO Pedestrian Detected")
